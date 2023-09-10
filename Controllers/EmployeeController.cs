0e9d587929c99cc12921b3c0d6af452573ff308f
macruz <macruz@LPT100115>
2023-09-09 23:37:40 -0600

    Agregar archivos de proyecto.

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,96 @@
+﻿using MVCAppSimple.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCAppSimple.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private EmpDBContext db = new EmpDBContext();
+        // GET: Employee
+        public ActionResult Index()
+        {
+            var employees = from e in db.Employees
+                            orderby e.ID
+                            select e;
+            return View(employees);
+        }
+
+        // GET: Employee/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Employee/Create
+        [HttpPost]
+        public ActionResult Create(Employee emp)
+        {
+            try
+            {
+                db.Employees.Add(emp);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Employee/Edit/5
+        public ActionResult Edit(int id)
+        {
+
+            var employee = db.Employees.Single(m => m.ID == id);
+            return View(employee);
+        }
+
+        // POST: Employee/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                var employee = db.Employees.Single(m => m.ID == id);
+                if (TryUpdateModel(employee))
+                {
+                    //To Do:- database code
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+                return View(employee);
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Employee/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Employee/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        
+    }
+}
